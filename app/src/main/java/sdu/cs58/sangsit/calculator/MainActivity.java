7cd949231100b32aaf98fa003bb052a01453cd4e
sangsit <boomloveevena@gmail.com>
2018-02-21 11:43:34 +0700

    ADD Complete

---
+++ app/src/main/java/sdu/cs58/sangsit/calculator/MainActivity.java
@@ -0,0 +1,41 @@
+package sdu.cs58.sangsit.calculator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    //ประกาศตัวแปร
+    EditText number1EditText, number2EditText;
+    Button addButton;
+    TextView resultTextView;
+    int num1, num2, result;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //2. ผูกตัวแปรบน java ใช้คู่กับ XML
+        number1EditText = findViewById(R.id.editNumber1);
+        number2EditText = findViewById(R.id.editNumber2);
+        addButton = findViewById(R.id.btnAdd);
+        resultTextView = findViewById(R.id.txvResult);
+
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // แปลงค่าที่รับจาก EditText เป็นตัวเลข แล้วเก็บลงในตัวแปลชนิด int
+                num1 = Integer.parseInt(number1EditText.getText().toString().trim());
+                num2 = Integer.parseInt(number2EditText.getText().toString().trim());
+                result = num1 + num2;
+
+                //แสดงผลลัพท์การคำนวน
+                resultTextView.setText(result + "");
+            }
+        }); // end setOnClickListener
+    } //end method on Create
+} //end Class
